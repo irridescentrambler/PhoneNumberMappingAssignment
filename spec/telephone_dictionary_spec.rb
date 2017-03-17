bf4b3cf45fa3b5a5f09d2d66786de135858c47e8
shobhit-neo <shobhit.shukla@wwindia.com>
2017-03-17 19:25:25 +0530

    Indentation fix

--- spec/telephone_dictionary_spec.rb
+++ spec/telephone_dictionary_spec.rb
@@ -2,24 +2,24 @@ require_relative "spec_helper.rb"
 require_relative "../telephone_dictionary.rb"
 
 RSpec.describe do
-	describe "Tests the TelephoneDictionary class" do
-		before(:all) do
-			@telephone_dictionary = TelephoneDictionary.new
-		end
+  describe "Tests the TelephoneDictionary class" do
+    before(:all) do
+      @telephone_dictionary = TelephoneDictionary.new
+    end
 
-		it "Tests the initialize method" do
-			expect(@telephone_dictionary).to_not be_nil
-		end
+    it "Tests the initialize method" do
+      expect(@telephone_dictionary).to_not be_nil
+    end
 
-		it "Tests the instance variables" do
-			expect(@telephone_dictionary.instance_variable_get(:@telephone_number_map)).to_not be_nil
-			expect(@telephone_dictionary.instance_variable_get(:@isValid)).to_not be_nil
-		end
+    it "Tests the instance variables" do
+      expect(@telephone_dictionary.instance_variable_get(:@telephone_number_map)).to_not be_nil
+      expect(@telephone_dictionary.instance_variable_get(:@isValid)).to_not be_nil
+    end
 
-		it "Tests the #find_mathing_combination_of_digit method" do
-			@telephone_dictionary.instance_variable_set(:@str, "6686787825")
-			expect(@telephone_dictionary.find_matching_combination_of_digit).to eq([["motor", "truck"], ["motor", "usual"], ["noun", "struck"], ["nouns", "truck"], ["nouns", "usual"], ["onto", "struck"], "motortruck"])
-		end
-	end
+    it "Tests the #find_mathing_combination_of_digit method" do
+      @telephone_dictionary.instance_variable_set(:@str, "6686787825")
+      expect(@telephone_dictionary.find_matching_combination_of_digit).to eq([["motor", "truck"], ["motor", "usual"], ["noun", "struck"], ["nouns", "truck"], ["nouns", "usual"], ["onto", "struck"], "motortruck"])
+    end
+  end
 end
 
--- telephone_dictionary.rb
+++ telephone_dictionary.rb
@@ -1,35 +1,35 @@
 require 'set'
 
 class TelephoneDictionary
-	attr_accessor :telephone_number_map, :str, :isValid
-	def initialize
-		@telephone_number_map = {
-			'2': ['a', 'b', 'c'],
-			'3': ['d', 'e', 'f'],
-			'4': ['g', 'h', 'i'],
-			'5': ['j', 'k', 'l'],
-			'6': ['m', 'n', 'o'],
-			'7': ['p', 'q', 'r', 's'],
-			'8': ['t', 'u', 'v'],
-			'9': ['w', 'x', 'y', 'z']
-		}
-		@isValid = true
-	end
+  attr_accessor :telephone_number_map, :str, :isValid
+  def initialize
+    @telephone_number_map = {
+      '2': ['a', 'b', 'c'],
+      '3': ['d', 'e', 'f'],
+      '4': ['g', 'h', 'i'],
+      '5': ['j', 'k', 'l'],
+      '6': ['m', 'n', 'o'],
+      '7': ['p', 'q', 'r', 's'],
+      '8': ['t', 'u', 'v'],
+      '9': ['w', 'x', 'y', 'z']
+    }
+    @isValid = true
+  end
 
-	def get_input
-		begin
-			puts "Enter a 10 digit[2-9] mobile number."
-			@str = gets.chomp
-			if /\A[2-9]{10}\z/.match(@str)
-				puts "valid"
-				@isValid = false
-			else
-				puts "Please enter a valid number"
-			end
-		end while(@isValid)
-	end
+  def get_input
+    begin
+      puts "Enter a 10 digit[2-9] mobile number."
+      @str = gets.chomp
+      if /\A[2-9]{10}\z/.match(@str)
+        puts "valid"
+        @isValid = false
+      else
+        puts "Please enter a valid number"
+      end
+    end while(@isValid)
+  end
 
-	def find_matching_combination_of_digit
+  def find_matching_combination_of_digit
 # Create new array of telephone number using telephone_number_map
 new_telphone_destionary = @str.scan(/[2-9]/).map { |a| @telephone_number_map[:"#{a}"] }
 
@@ -42,11 +42,11 @@ dict_array = File.read('dictionary.txt').downcase.split("\n").to_set
 # Find out output according to assignment
 result = []
 combination_of_digit.each do |com_of_digit|
-	5.times do |i|
-		if (dict_array.include? com_of_digit[0..i+2]) && (dict_array.include? com_of_digit[i+3..9])
-			result << [com_of_digit[0..i+2],com_of_digit[i+3..9]]
-		end
-	end
+  5.times do |i|
+    if (dict_array.include? com_of_digit[0..i+2]) && (dict_array.include? com_of_digit[i+3..9])
+      result << [com_of_digit[0..i+2],com_of_digit[i+3..9]]
+    end
+  end
 end
 (combination_of_digit & dict_array).to_a.each { |matching_array| result << matching_array }
 result.to_a.compact
